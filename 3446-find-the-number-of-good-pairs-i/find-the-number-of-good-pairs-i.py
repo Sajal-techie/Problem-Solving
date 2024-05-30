64bd715606dbc69a2b08e7ca08ecd38a629c0d49
Sajal K <147618636+Sajal-techie@users.noreply.github.com>
2024-05-30 09:50:22 +0530

    Time: 24 ms (96.61%) | Memory: 11.7 MB (19.39%) - LeetSync

--- 3446-find-the-number-of-good-pairs-i/find-the-number-of-good-pairs-i.py
+++ 3446-find-the-number-of-good-pairs-i/find-the-number-of-good-pairs-i.py
@@ -1,8 +1,15 @@
-class Solution:
-    def numberOfPairs(self, nums1: List[int], nums2: List[int], k: int) -> int:
+class Solution(object):
+    def numberOfPairs(self, nums1, nums2, k):
+        """
+        :type nums1: List[int]
+        :type nums2: List[int]
+        :type k: int
+        :rtype: int
+        """
         count = 0
         for i in nums1:
             for j in nums2:
                 if i % (j * k) == 0:
                     count += 1
         return count
+        
