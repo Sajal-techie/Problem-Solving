1c98d8aa135329ff13c56971098df016e29d16ff
Sajal K <147618636+Sajal-techie@users.noreply.github.com>
2024-06-01 09:18:19 +0530

    Time: 339 ms (75.00%) | Memory: 147.4 MB (83.33%) - LeetSync

---
+++ 3379-score-of-a-string/score-of-a-string.dart
@@ -0,0 +1,9 @@
+class Solution {
+  int scoreOfString(String s) {
+    int ans = 0;
+    for (int i =0;i<s.length-1;i++){
+        ans += (s.codeUnitAt(i) - s.codeUnitAt(i+1)).abs() ;
+    }
+    return ans;
+  }
+}
