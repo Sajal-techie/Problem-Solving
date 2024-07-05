44159df8e448d19d360edaca161d7b65db50b33e
Sajal K <147618636+Sajal-techie@users.noreply.github.com>
2024-07-05 10:25:13 +0530

    Time: 59 ms (88.00%) | Memory: 17 MB (76.07%) - LeetSync

---
+++ 871-keys-and-rooms/keys-and-rooms.py
@@ -0,0 +1,14 @@
+class Solution:
+    def canVisitAllRooms(self, rooms: List[List[int]]) -> bool:
+        seen = [False] * len(rooms)
+        seen[0] = True
+        queue = [0]
+        while queue:
+            vertex = queue.pop(0)
+            for i in rooms[vertex]:
+                if not seen[i]:
+                    seen[i] = True
+                    queue.append(i)
+        return all(seen)
+
+            
