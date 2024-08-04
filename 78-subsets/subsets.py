af9087d97989f7da24c3ff903a1743811c7c2e4f
Sajal K <147618636+Sajal-techie@users.noreply.github.com>
2024-08-04 15:04:56 +0530

    Time: 39 ms (44.86%) | Memory: 16.6 MB (89.91%) - LeetSync

---
+++ 78-subsets/subsets.py
@@ -0,0 +1,6 @@
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        result = [[]]
+        for i in nums:
+            result += [ j +[i] for j in result]
+        return result
