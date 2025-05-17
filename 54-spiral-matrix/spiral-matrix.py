00314ea32f2e3926b9789be3ab421ee1f2647060
Sajal K <147618636+Sajal-techie@users.noreply.github.com>
2025-05-17 17:58:13 +0530

    Time: 0 ms (100.00%) | Memory: 17.7 MB (88.89%) - LeetSync

---
+++ 54-spiral-matrix/spiral-matrix.py
@@ -0,0 +1,35 @@
+class Solution:
+    def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
+        n = len(matrix)
+        m = len(matrix[0])
+        res = []
+        visited = set()
+        i, j = 0, 0 
+        dr = "R"
+        print(matrix, n, m)
+        while len(res) < n * m:
+            if (i,j) not in visited:
+                visited.add((i, j))
+                res.append(matrix[i][j])
+            if dr == "R":
+                if j+1 < m and (i, j+1) not in visited:
+                    j += 1
+                else:
+                    dr = "D"
+            elif dr == "D":
+                if i+1 < n and (i+1, j) not in visited:
+                    i += 1
+                else:
+                    dr = "L"
+            elif dr == "L":
+                if j > 0 and (i, j-1) not in visited:
+                    j -= 1
+                else:
+                    dr = "U"
+            elif dr == "U":
+                if i > 0 and (i-1, j) not in visited:
+                    i -= 1
+                else:
+                    dr = "R"
+
+        return res
