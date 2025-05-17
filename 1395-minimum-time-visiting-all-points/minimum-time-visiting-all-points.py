16ed8b7db1733a46e13ebb69477091ce52e1df68
Sajal K <147618636+Sajal-techie@users.noreply.github.com>
2025-05-17 17:26:41 +0530

    Time: 3 ms (35.22%) | Memory: 18 MB (25.48%) - LeetSync

--- 1395-minimum-time-visiting-all-points/minimum-time-visiting-all-points.py
+++ 1395-minimum-time-visiting-all-points/minimum-time-visiting-all-points.py
@@ -3,7 +3,6 @@ class Solution:
         res = 0
         first = points.pop(0)
         for i in points:
-            temp = max(abs(i[0]-first[0]), abs(i[1]-first[1]))
+            res += max(abs(i[0]-first[0]), abs(i[1]-first[1]))
             first = i
-            res += temp
         return res
