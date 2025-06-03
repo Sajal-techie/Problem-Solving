ad6af5de4054725512818b29fd94939f5f32a8a2
Sajal K <147618636+Sajal-techie@users.noreply.github.com>
2025-06-04 01:36:22 +0530

    Time: 107 ms (38.80%) | Memory: 28.5 MB (67.44%) - LeetSync

--- 11-container-with-most-water/container-with-most-water.py
+++ 11-container-with-most-water/container-with-most-water.py
@@ -1,15 +1,15 @@
 class Solution:
     def maxArea(self, height: List[int]) -> int:
-        n = len(height)-1
-        res = 0
-        l = 0
-        r = n
-        while l < r:
-            minval = min(height[l],height[r])
-            area = (r-l) * minval
-            res = max(area, res)
-            if height[l] <= height[r]: 
+        res = 0 # result 
+        l = 0  # left index
+        r = len(height) - 1 #right index
+        while l < r:  # limiting condition 
+            minval = min(height[l],height[r]) # store the minimum value from right and left
+            area = (r-l) * minval # find the current area using minvalue and difference of indexes
+            res = max(area, res) # find the maximun from current area and previos stored result
+            if height[l] <= height[r]: # increase or decresase index based on the condition
                 l+=1 
             else:
                 r-=1
-        return res
+        return res # res will now have the maximunm area 
+        
