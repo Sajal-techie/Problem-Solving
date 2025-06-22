1ede878ca3490d92df7f9524f6739430c5de40d3
Sajal K <147618636+Sajal-techie@users.noreply.github.com>
2025-06-23 00:31:05 +0530

    Time: 0 ms (100.00%) | Memory: 18 MB (17.89%) - LeetSync

--- 2260-divide-a-string-into-groups-of-size-k/divide-a-string-into-groups-of-size-k.py
+++ 2260-divide-a-string-into-groups-of-size-k/divide-a-string-into-groups-of-size-k.py
@@ -1,14 +1,13 @@
 class Solution:
     def divideString(self, s: str, k: int, fill: str) -> List[str]:
-        res = []
         ls = list(s)
+        res = []
         temp = ""
         for i in range(len(ls)):
-            if i % k == 0:
+            if temp and i % k == 0:
                 res.append(temp)
                 temp = ""
             temp += ls[i]
-        res.pop(0)
         print(temp, res)
         while len(temp) < k:
             temp += fill
