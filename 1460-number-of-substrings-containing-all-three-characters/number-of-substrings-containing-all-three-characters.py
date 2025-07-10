2c20b1edef59c9459bc452009c05a29e9afee393
Sajal K <147618636+Sajal-techie@users.noreply.github.com>
2025-07-11 01:03:51 +0530

    Time: 87 ms (82.05%) | Memory: 17.7 MB (98.54%) - LeetSync

--- 1460-number-of-substrings-containing-all-three-characters/number-of-substrings-containing-all-three-characters.py
+++ 1460-number-of-substrings-containing-all-three-characters/number-of-substrings-containing-all-three-characters.py
@@ -3,9 +3,7 @@ class Solution:
         map = {"a": -1, "b": -1, "c": -1}
         res = 0
         for i in range(len(s)):
-            if i > map.get(s[i]):
-                map[s[i]] = i
-            if map.get("a") != -1 and map.get("b") != -1 and map.get("c") != -1:
-                temp = min(map.get("a"), map.get("b"), map.get("c"))  + 1
-                res += temp
+            map[s[i]] = i
+            temp = min(map.values())  + 1
+            res += temp
         return res
