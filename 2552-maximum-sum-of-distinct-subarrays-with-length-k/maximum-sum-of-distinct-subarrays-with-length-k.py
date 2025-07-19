e87fab11f53ce49323ad414d6067fad6db977e7e
Sajal K <147618636+Sajal-techie@users.noreply.github.com>
2025-07-20 01:13:13 +0530

    Time: 86 ms (93.28%) | Memory: 34.5 MB (89.37%) - LeetSync

--- 2552-maximum-sum-of-distinct-subarrays-with-length-k/maximum-sum-of-distinct-subarrays-with-length-k.py
+++ 2552-maximum-sum-of-distinct-subarrays-with-length-k/maximum-sum-of-distinct-subarrays-with-length-k.py
@@ -1,24 +1,23 @@
 class Solution:
     def maximumSubarraySum(self, nums: List[int], k: int) -> int:
-        res = 0
-        table = defaultdict(int)
-        temp = 0
-        print(table)
-        for i in range(k):
-            table[nums[i]] += 1
-            temp += nums[i]
-        # print(table)
-        if len(table) == k:
-            res = temp
-        print(res, temp)
-        for i in range(k, len(nums)):
-            table[nums[i]] += 1
-            table[nums[i-k]] -= 1
-            temp += nums[i]
-            temp -= nums[i-k]
-            if table[nums[i-k]] == 0:
-                del table[nums[i-k]]
-            if len(table) == k:
-                res = max(temp, res)
-        print(temp)
-        return res
+        curr =  max_sum = l = 0
+        elem = set()
+        for i in range(len(nums)):
+            if nums[i] not in elem:
+                elem.add(nums[i])
+                curr += nums[i]
+                
+                if i - l + 1 == k:
+                    if curr > max_sum:
+                        max_sum = curr
+                    elem.remove(nums[l])
+                    curr -= nums[l]
+                    l += 1
+            else:
+                while nums[i] in elem:
+                    elem.remove(nums[l])
+                    curr -= nums[l]
+                    l += 1
+                elem.add(nums[i])
+                curr += nums[i]
+        return max_sum
