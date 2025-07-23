d07c4520485d5c1a50a02bad19170decef5f070c
Sajal K <147618636+Sajal-techie@users.noreply.github.com>
2025-07-24 00:33:56 +0530

    Time: 131 ms (99.35%) | Memory: 18 MB (96.13%) - LeetSync

--- 1818-maximum-score-from-removing-substrings/maximum-score-from-removing-substrings.py
+++ 1818-maximum-score-from-removing-substrings/maximum-score-from-removing-substrings.py
@@ -1,45 +1,22 @@
 class Solution:
     def maximumGain(self, s: str, x: int, y: int) -> int:
-        res =  0
-        ls = list(s)
-        print(ls)
-        n = len(ls)
-        s1 = [ls[0]]
-        i = 1
-        while i < n:
-            curr = ls[i]
-            if x >= y:
-                if s1 and s1[-1] == 'a' and curr == 'b':
-                    s1.pop()
+        a, b = 'a', 'b'
+        if x < y:
+            x, y = y, x
+            a,b = b, a
+
+        res = c1 = c2 = 0
+        for c in s:
+            if c == a:
+                c1 += 1
+            elif c == b:
+                if c1:
+                    c1 -= 1
                     res += x
                 else:
-                    s1.append(curr)
+                    c2 += 1
             else:
-                if s1 and s1[-1] == 'b' and curr == 'a':
-                    s1.pop()
-                    res += y
-                else:
-                    s1.append(curr)
-            i += 1
-        print('s1', s1)
-        if not s1:
-            return res
-        i = 1
-        s2 = [s1[0]]
-        while i < len(s1):
-            curr = s1[i]
-            # print(i, curr, s2, res)
-            if x >= y:
-                if s2 and s2[-1] == 'b' and curr == 'a':
-                    s2.pop()
-                    res += y
-                else:
-                    s2.append(curr)
-            else:
-                if s2 and s2[-1] == 'a' and curr =='b':
-                    s2.pop()
-                    res += x
-                else:
-                    s2.append(curr)
-            i += 1
+                res += min(c1, c2) * y
+                c1 = c2 = 0
+        res += min(c1, c2) * y
         return res
