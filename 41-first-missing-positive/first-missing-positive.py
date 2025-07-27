10d616de69f3e5617b3b512974cd9f8487d90d47
Sajal K <147618636+Sajal-techie@users.noreply.github.com>
2025-07-27 19:07:51 +0530

    Time: 15 ms (97.06%) | Memory: 29.3 MB (36.54%) - LeetSync

--- 41-first-missing-positive/first-missing-positive.py
+++ 41-first-missing-positive/first-missing-positive.py
@@ -1,27 +1,12 @@
 class Solution:
     def firstMissingPositive(self, nums: List[int]) -> int:
-        # Function to swap elements at indices i and j
-        def swap_elements(index1, index2):
-            nums[index1], nums[index2] = nums[index2], nums[index1]
-
-        # Get the length of the list
-        list_length = len(nums)
-      
-        # Iterating through the list to place numbers on their correct positions
-        for i in range(list_length):
-            # Continuously swap the current element until it's in its correct position
-            # or it's out of range [1, n]
-            while 1 <= nums[i] <= list_length and nums[i] != nums[nums[i] - 1]:
-                swap_elements(i, nums[i] - 1)
-              
-        # After placing each element in its correct position, or as correct as possible, 
-        # traverse the list to find the first missing positive integer
-        for i in range(list_length):
-            # If the current number isn't the right number at index i, return i + 1,
-            # because it is the first missing positive integer
-            if i + 1 != nums[i]:
-                return i + 1
-      
-        # If all previous positions contain the correct integers, 
-        # then the first missing positive integer is n + 1 
-        return list_length + 1
+    
+        n = len(nums)
+        f = [False] * (n + 1)
+        for i in nums:
+            if i > 0 and i <= n:
+                f[i] = True
+        for j in range(1, n + 1):
+            if not f[j]:
+                return j
+        return n + 1 
