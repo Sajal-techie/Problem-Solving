f1e4f161e1342c2d7711af71685904eb61c88974
Sajal K <147618636+Sajal-techie@users.noreply.github.com>
2025-08-13 01:13:22 +0530

    Time: 1 ms (39.51%) | Memory: 18 MB (24.65%) - LeetSync

--- 82-remove-duplicates-from-sorted-list-ii/remove-duplicates-from-sorted-list-ii.py
+++ 82-remove-duplicates-from-sorted-list-ii/remove-duplicates-from-sorted-list-ii.py
@@ -1,26 +1,17 @@
-# Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
 class Solution:
     def deleteDuplicates(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        res = ListNode(0)
-        dummy = res
-        curr = head
-        excluded = set()
-        while curr:
-            if curr.next:
-                if curr.val != curr.next.val and curr.val not in excluded:
-                    node = ListNode(curr.val)
-                    dummy.next = node
-                    dummy = dummy.next
-                else:
-                    excluded.add(curr.val)
-            elif curr.val not in excluded:
-                node = ListNode(curr.val)
-                dummy.next = node
-                dummy = dummy.next
-            
-            curr = curr.next if curr else None
-        return res.next
+        dummy = ListNode(-1)
+        dummy.next = head
+        prev = dummy
+        cur = head
+
+        while cur and cur.next:
+            if cur.val == cur.next.val:
+                while cur.next and cur.val == cur.next.val:
+                    cur = cur.next
+                prev.next = cur.next  # Skip all duplicates
+            else:
+                prev = prev.next  # Move to next distinct node
+            cur = cur.next
+
+        return dummy.next
